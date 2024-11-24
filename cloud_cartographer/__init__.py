40aaae8a65c2b5f6baa98cc38c4f3f45aafd6b49
Vincent Van Gestel <vincent.vangestel@vrt.be>
2024-11-24 19:24:55 +0100

    Initial commit with project structure

---
+++ cloud_cartographer/__init__.py
@@ -0,0 +1,9 @@
+"""Cloud Cartographer.
+
+Main entrypoint module for the ccarto tool.
+"""
+
+
+def main():
+    """Entry point for the application script."""
+    print("Call your main application code here")
